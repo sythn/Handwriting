b019df310bba2e04c839e5c47028b8a55e06435d
Seyithan Teymur <sth@Sths-MacBook-2.local>
2011-05-12 14:58:09 +0300

    Initial Commit

---
+++ Handwriting/HandwritingAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  HandwritingAppDelegate.h
+//  Handwriting
+//
+//  Created by Seyithan Teymur on 5/12/11.
+//  Copyright 2011 JoopleBerry Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HandwritingAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ Handwriting/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Handwriting
+//
+//  Created by Seyithan Teymur on 5/12/11.
+//  Copyright 2011 JoopleBerry Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	int retVal = UIApplicationMain(argc, argv, nil, nil);
+	[pool release];
+	return retVal;
+}
